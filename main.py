5cbcca8a16d09b71a9a2d57bb6b4cf4cef09b6a4
Tetris8655 <urgggggggg@urgg.urg>
2025-08-17 00:44:02 +0300

    optimized print usage using batch printing

--- main.py
+++ main.py
@@ -19,20 +19,15 @@ def main():
     num_words = get_num_words(get_book_text(filepath))
     num_chars = get_num_chars(get_book_text(filepath))
     chars_sorted = sort_chars(num_chars)
-    #print(num_words)
-    #print(num_chars)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
+    header_text = f"============ BOOKBOT ============\nAnalyzing book found at {filepath}...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------"
+    print(header_text)
 
+    lines = []
     for item in chars_sorted:
         if item["char"].isalpha():
-            print(f"{item["char"]}: {item["num"]}")
+            lines.append(f"{item["char"]}: {item["num"]}")
 
-    print("============= END ===============")
-
-    return 0
+    lines.append("============= END ===============")
+    print('\n'.join(lines))
 
 main()
